b1d583ed892d538b3285f3e66cf201f141022fcf
Shaik Kamil <shaikm.kamil@gmail.com>
2021-12-16 18:31:14 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function add (a,b) {
+    return a + b;
+}
+function subtract (a,b) {
+    return a - b;
+}
+function multiply (a,b) {
+    return a * b;
+}
+function divide (a,b) {
+    return a / b;
+}
+function increment (n) {
+    return n+1;
+}
+function decrement (n){
+    return n-1;
+}
+function makeInt(n){
+    parseInt(n)
+    return n;
+}
+function makeInt(a,base){
+    let parsed = parseInt(a,10)
+     parseInt(a,10)
+     return a;
+}
+function makeInt(b){
+    parseInt("sldkjflksjf");
+    return b;
+}
+function preserveDecimal(c){
+    return parseFloat(c,10);
+}
